9ce7fa2f7763f1918ee88369c2e76917a1814526
archis1405 <archismandas1405@gmail.com>
2025-10-24 02:28:25 +0530

    ADDED : The User entity class

---
+++ src/main/java/com/example/RecipeBook/entity/User.java
@@ -0,0 +1,59 @@
+package com.example.RecipeBook.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @Column(nullable = false)
+    private Boolean emailVerified = false;
+
+    @CreationTimestamp
+    @Column(updatable = false , nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public enum Role{
+        USER,
+        CHEF,
+        ADMIN
+    }
+}
