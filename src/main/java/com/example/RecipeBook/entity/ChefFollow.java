a27e9924d332441a318f7ac1807effebd80fdce7
archis1405 <archismandas1405@gmail.com>
2025-10-24 03:25:37 +0530

    ADDED : The Follower entity class

--- src/main/java/com/example/RecipeBook/entity/ChefFollow.java
+++ src/main/java/com/example/RecipeBook/entity/ChefFollow.java
@@ -1,4 +1,38 @@
 package com.example.RecipeBook.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Table(
+        name = "chef_follows",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"follower_id", "following_id"})
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChefFollow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private Chef follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id", nullable = false)
+    private Chef following;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
 }
