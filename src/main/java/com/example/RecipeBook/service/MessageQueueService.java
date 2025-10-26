637cd36a19b2ff63bb391b503b0a91eb33c7fe2f
archis1405 <archismandas1405@gmail.com>
2025-10-26 19:59:15 +0530

    ADDED : The Messaging Queue Service Class

---
+++ src/main/java/com/example/RecipeBook/service/MessageQueueService.java
@@ -0,0 +1,33 @@
+package com.example.RecipeBook.service;
+
+import com.example.RecipeBook.dto.RecipeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MessageQueueService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing-key}")
+    private String routingKey;
+
+    public void sendRecipeForProcessing(RecipeMessage message ){
+        try{
+            rabbitTemplate.convertAndSend(exchange , routingKey , message);
+            log.info("Recipe is sent to queue for processing: {}", message.getRecipeId());
+        }
+        catch (Exception e){
+            log.error("Failed to send recipe to queue: {}" , e);
+            throw new RuntimeException("Failed to send recipe for processing" , e);
+        }
+    }
+}
